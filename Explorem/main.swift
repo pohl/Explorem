a36080cd33054f9c54bf953546c1330f93e2fd0e
Pohl Longsine <pohl.longsine@gmail.com>
2014-07-27 19:31:16 -0500

    Initial Commit

---
+++ Explorem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Explorem
+//
+//  Created by Pohl Longsine on 7/27/14.
+//  Copyright (c) 2014 the screaming organization. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
